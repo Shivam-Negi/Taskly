9238805304112cfff6e91d47b80079ee9687f741
Shivam Negi <shivam.negi@toddleapp.com>
2025-09-06 23:47:58 +0530

    feat: Add a completed UI for the shopping list component

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -3,9 +3,10 @@ import { theme } from "../theme";
 
 type Props = {
   name: string;
+  isCompleted?: boolean;
 };
 
-export function ShoppingListItem({ name }: Props) {
+export function ShoppingListItem({ name, isCompleted }: Props) {
   const handleDelete = () => {
     Alert.alert(
       `Are you sure you want to delete ${name}?`,
@@ -17,15 +18,20 @@ export function ShoppingListItem({ name }: Props) {
           onPress: () => console.log("Item deleted"),
         },
         { text: "Cancel", style: "cancel" },
-      ],
+      ]
     );
   };
 
   return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemText}>{name}</Text>
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text style={[styles.itemText, isCompleted ? styles.completedItemText : undefined]}>{name}</Text>
       <TouchableOpacity
-        style={styles.button}
+        style={[styles.button, isCompleted ? styles.completedButton : undefined]}
         onPress={handleDelete}
         activeOpacity={0.8}
       >
@@ -45,14 +51,26 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
   itemText: {
     fontSize: 18,
     fontWeight: 200,
+  },
+  completedItemText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorLightGrey,
+    color: theme.colorGrey,
   },
   button: {
     backgroundColor: theme.colorBlack,
     padding: 8,
     borderRadius: 6,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGrey,
   },
   buttonText: {
     color: theme.colorWhite,
--- theme.ts
+++ theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
   colorCerulean: "#1a759f",
-  colorWhite: "#ffffff",
-  colorBlack: "#000000",
+  colorWhite: "#fff",
+  colorBlack: "#000",
+  colorGrey: "grey",
+  colorLightGrey: "#eee",
 };
