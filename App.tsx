d4e30d7d78f0569417d8e8ccfdc637b96402bdc2
Shivam Negi <shivam.negi@toddleapp.com>
2025-09-06 22:18:29 +0530

    refac: ShoppingListItem to a reusable component

--- App.tsx
+++ App.tsx
@@ -1,35 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { theme } from "./theme";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert("Are you sure you want to delete this item?",
-      "This action cannot be undone.",
-      [
-        {
-          text: "Yes",
-          style: "destructive",
-          onPress: () => console.log("Item deleted"),
-        },
-        { text: "Cancel", 
-          style: "cancel"
-        },
-      ]
-    )
-  };
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={handleDelete}
-        activeOpacity={0.8}>
-        <Text style={styles.buttonText}>Delete</Text>
-      </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Tea" />
+      <ShoppingListItem name="Milk" />
     </View>
   );
 }
@@ -38,30 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: theme.colorCerulean,
-    paddingHorizontal: 16,
-    paddingVertical: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  itemText: {
-    fontSize: 18,
-    fontWeight: 200,
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    letterSpacing: 1,
+    justifyContent: "center",
   },
 });
--- theme.ts
+++ theme.ts
@@ -1,5 +1,5 @@
 export const theme = {
-    colorCerulean: "#1a759f",
-    colorWhite: "#ffffff",
-    colorBlack: "#000000",
-}
+  colorCerulean: "#1a759f",
+  colorWhite: "#ffffff",
+  colorBlack: "#000000",
+};
