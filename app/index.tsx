8835808c91df2bd0fba19627bb8efbd926ead8f2
Shivam Negi <shivam.negi@toddleapp.com>
2025-09-28 20:54:47 +0530

    data persistance in local storage

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet, TextInput, FlatList, View, Text } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getFromStorage, saveToStorage } from "../utils/storage";
+
+const storageKey = "shoppingList";
 
 type ShoppingListItemType = {
   id: string;
@@ -14,6 +17,16 @@ export default function App() {
   const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
   const [newItem, setNewItem] = useState("");
 
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const data = await getFromStorage(storageKey);
+      if (data) {
+        setShoppingList(data);
+      }
+    };
+    fetchInitialData();
+  }, []);
+
   const handleAddItem = () => {
     if (newItem) {
       const newShoppingList = [
@@ -25,12 +38,14 @@ export default function App() {
         ...shoppingList,
       ];
       setShoppingList(newShoppingList);
+      saveToStorage(storageKey, newShoppingList);
       setNewItem("");
     }
   };
 
   const handleDelete = (id: string) => {
     const newShoppingList = shoppingList.filter((item) => item.id !== id);
+    saveToStorage(storageKey, newShoppingList);
     setShoppingList(newShoppingList);
   };
 
@@ -48,6 +63,7 @@ export default function App() {
         return item;
       }
     });
+    saveToStorage(storageKey, newShoppingList);
     setShoppingList(newShoppingList);
   };
 
